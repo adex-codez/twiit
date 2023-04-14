af4ddc1f37cac55bc9d5dcc7c35f4ca6906157d6
Adegbite Adeadura <adetimilehin@gmail.com>
2023-04-14 10:47:57 +0100

    deleted some files from the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react"
-import Home from "./components/Home";
-import Menu from "./components/Menu";
-import Upload from "./components/Upload";
 
 
 function App() {
